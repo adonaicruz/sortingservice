8bf88f5e54cfc8791ab05da3f17790e6e267fdc0
Adonai Cruz <adonai.cruz@gmail.com>
2016-09-16 19:42:01 -0300

    0.0.1

---
+++ example/index.php
@@ -0,0 +1,123 @@
+<?php
+	require '../lib/SortingService.php';
+	
+	//loading json data to be sorted
+	$json_data = file_get_contents("test.json");
+
+	//Create a var with the orinal data to compare
+	$vDataOrigin = json_decode($json_data, true);
+	
+	$ss = new SortingService($json_data);
+	
+	$vHeader = $ss->getHeader();
+
+	if($_POST):
+		foreach($_POST['FIELD'] as $k=>$column):
+			if($column !== ''){
+				$sorter[$column] = $_POST['SORT'][$k];
+			}
+		endforeach;
+		
+
+		if(count($sorter)){
+			//sorts the data according to the specified parameters
+			$ss->sort($sorter);
+
+			$vDataSorted = $ss->getData();
+		}
+		
+	endif;
+	
+	
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Security-Policy">
+		<meta charset="utf-8">
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+		<title>Sorting Service Client Example</title>
+		
+		<!-- Bootstrap -->
+    	<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+    	<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+    	<style>
+    		.wrap{
+				width: 700px;
+				margin:0 auto;
+    		}
+    	</style>
+	</head>
+	<body>
+
+	<div class="wrap">
+		<form action="" method="POST">
+			<h1>Sorting Data</h1>
+			<?php for($i=1;$i<=count($vHeader);$i++):?>
+				<div class="form-inline">
+					<div class="form-group">
+						<label for="FIELD">Column</label>
+						<select name="FIELD[<?php echo $i;?>]" class="form-control">
+							<option value=""></option>
+							<?php foreach($vHeader as $k=>$header):?>
+								<option value="<?php echo $k;?>"><?php echo $header;?></option>
+							<?php endforeach;?>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="SORT">Order</label>
+						<select name="SORT[<?php echo $i;?>]" class="form-control" >
+							<option value="asc">ASC</option>
+							<option value="desc">DESC</option>
+						</select>
+					</div>
+			</div>
+			<?php endfor;?>
+			<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+
+
+		<h3>Original Data</h3>
+		<div class="panel panel-default" >
+		  	<table class="table table-striped">
+		  		<?php foreach($vDataOrigin as $k=>$data_row):?>
+			  		<tr>
+		  				<?php foreach($data_row as $data_col):?>
+			  				<td><?php echo $data_col?></td>
+			  			<?php endforeach;?>
+			  		</tr>
+			  	<?php endforeach;?>
+		  	</table>
+		</div>
+		
+		<?php if(isset($vDataSorted) && count($vDataSorted)):?>
+		  	<h3>Sorted Data</h3>
+
+			<?php if($sorter):?>
+				Table sorted: 
+				<?php foreach($sorter as $sColumn=>$sOrder):?>
+					<br/>Column: <?php echo $vHeader[$sColumn];?> | Order: <?php echo $sOrder;?>
+				<?php endforeach;?>
+			<?php endif;?>
+
+		  	<div class="panel panel-default" >
+			  	<table class="table table-striped">
+			  		<?php foreach($vDataSorted as $k=>$data_row):?>
+				  		<tr>
+			  				<?php foreach($data_row as $data_col):?>
+				  				<td><?php echo $data_col?></td>
+				  			<?php endforeach;?>
+				  		</tr>
+				  	<?php endforeach;?>
+			  	</table>
+			</div>
+		<?php endif;?>
+	</div>
+		
+		
+	</body>
+
+	
+	
+</html>
---
+++ lib/SortingService.php
@@ -0,0 +1,117 @@
+<?php
+/*!
+ * Sorting Service Class
+ * 
+ * @version     0.0.1
+ * @author adonaicruz <adonai.cruz@gmail.com>
+ * @license This software is licensed under the MIT license: http://opensource.org/licenses/MIT
+ * 
+ */
+
+
+class SortingService {
+	protected $data,$header;
+	var $sort;
+
+	 /**
+     * Create instance and load data
+     *
+     * @param null|json|array   $data   Table data to be sorted
+     *
+     * @return SS
+     * @throws Exception
+     *
+     */
+    function __construct($data) {
+    	if($data === null){
+    		throw new Exception('Table data can not be null .');
+    	}else{
+            $this->load($data);
+        }
+        return $this;
+    }
+
+     /**
+     * Load table data
+     *
+     * @param json|array   $data   Table data to be sorted
+     * @param bolean       $header It says if the table has headers
+     * @return array
+     * @throws Exception
+     *
+     */
+    function load($data,$header=true) {
+
+        if(is_string($data)){
+        	$data = json_decode($data);
+        	if(json_last_error() !== JSON_ERROR_NONE){
+        		throw new Exception('Invalid json data.');
+        	}
+        }
+        if($header){
+        	$this->header = $data[0];
+        	$data = array_slice($data, 1);
+        	if(!count($data)){
+        		// throw new Exception('Vazio.');
+        	}
+        }
+        $this->data = $data;
+        return $this->data;
+    }
+    
+    /**
+     * Get table data
+     *
+     * @return array
+     *
+     */
+    public function getData() {
+    	return $this->data;
+    }
+
+    /**
+     * Get table header
+     *
+     * @return array
+     *
+     */
+    public function getHeader() {
+    	return $this->header;
+    }
+
+    /**
+     * Method to Sort Array
+     *
+     * @param  array   $sort   array('column'=>'order') - Column=[0,1,n], order=[asc,desc]
+     * @return array
+     *
+     */
+    public function sort(array $sort) {
+    	$this->sort = $sort;
+    	usort($this->data, array($this, 'check'));
+    	return $this->data;
+    }
+
+    /**
+     * Helper to check the data order. Compare 2 rows [a,b] and check the position
+     *
+     * @return array
+     *
+     */
+	protected function check($a, $b){
+		foreach($this->sort as $column=>$order):
+			if ($a[$column] == $b[$column]) {
+				$return = 0;
+			}else{
+				if($order == "desc"){
+					return ($a[$column] > $b[$column]) ? -1 : 1;
+				}else{
+					return ($a[$column] < $b[$column]) ? -1 : 1;
+				}
+			}
+		endforeach;
+
+		return $return;
+		
+	}
+}
