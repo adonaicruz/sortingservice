2c733d411e188a0b0ad6592b6f865366df003072
Adonai Cruz <adonai.cruz@gmail.com>
2016-09-16 15:37:05 -0300

    start files

---
+++ lib/sorting_service.php
@@ -0,0 +1,18 @@
+<?php
+/*!
+ * Sorting Service Class
+ * 
+ * 
+ * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
+ * 
+ * @author adonaicruz
+ * https://github.com/adonaicruz
+ * 
+ */
+
+
+class SS {
+   const version = "0.0.1";
+
+
+ }
